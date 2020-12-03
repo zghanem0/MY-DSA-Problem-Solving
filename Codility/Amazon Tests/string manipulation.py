b7f40ca12ce16fd46125a74c62e097092b95c517
pro-ghanem <holywater150@gmail.com>
2020-12-03 14:36:55 +0200

    Update string manipulation.py

--- Codility/Amazon Tests/string manipulation.py
+++ Codility/Amazon Tests/string manipulation.py
@@ -13,3 +13,21 @@ def solution(S):
 S = "we test. coders! Give us a try?"
 print(solution(S))
 #print(re.split('\.|\*|\?|!', S))
+
+
+# Another Solution by unifing the delimiters: using replace method : replace word by word >> <sentance>.replace('old-word','new-word') 
+
+def solution(S):
+    larger=0
+    replace1=S.replace('?','!')
+    replace2=replace1.replace('.','!')
+    sentence=replace2.split('!')
+    print(sentence)
+    for sent in sentence:
+        splited_sent=sent.split()
+        words_count=len(splited_sent)
+        larger=max(larger,words_count)
+    return larger
+
+S = "we test. coders! Give us a try?"
+print(solution(S))
