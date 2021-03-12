6d00e71e9a380084e0ef91b4c14e768970be5785
pro-ghanem <pro.ahmedghanem@gmail.com>
2021-03-12 21:38:55 +0200

    Update Bi-Valued.py

--- Codility/Amazon Tests/Bi-Valued.py
+++ Codility/Amazon Tests/Bi-Valued.py
@@ -1,3 +1,40 @@
+
+#A = [1,2,3,2]
+A = [16,17,4, 2, 3,2, 4,9, 2, 6, 8,13]
+#A=[1,2,3,2,5,6]
+#A=[2,2]
+def BiValued(A):
+    dic={}
+    current_bivalued=0
+    longest_bivalued=0
+
+    for i in A:
+        if i in dic:
+            dic[i]=1
+        else:
+            dic[i]=0
+    for a in A:
+        if dic[a]==1:
+            current_bivalued += 1
+            longest_bivalued=max(current_bivalued,longest_bivalued)
+        elif longest_bivalued==0:
+            continue
+        else:
+            current_bivalued+=1
+        print(current_bivalued, longest_bivalued)
+
+    return longest_bivalued
+print(BiValued(A))
+
+
+
+
+
+
+# ------------------------------- another one with dic -------------------------------------------------------------
+
+
+
 #A = [1,2,3,2]
 A=[4,2,2,4,2,6,8]
 #A=[1,2,3,2,5,6]
@@ -24,7 +61,7 @@ def BiValued(A):
 
 print(BiValued(A))
 
-# --------------------------- other prety awsome solution ------------------------
+# --------------------------- other prety awsome solution with array ------------------------
 
 # A = [1,2,3,2]
 #A = [4, 2, 2, 4, 2, 6, 8]
