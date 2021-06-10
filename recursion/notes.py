235ca86f0fc19233235a08f66affdc4ab8939a0f
pro-ghanem <pro.ahmedghanem@gmail.com>
2021-06-10 05:21:16 +0200

    Update notes.py

--- recursion/notes.py
+++ recursion/notes.py
@@ -8,6 +8,9 @@ so every recursive function need to have sth called a base case(stop point)
 - The first is when recursion is used as a technique in which a function makes one or more calls to itself.
 - The second is when a data structure uses smaller instances of the exact same type of data structure when it represents itself.
 
+>> there are 2 types of stacks: either using my own stack, or call stack (return function_name) again
+
+>> 
 a,b=b,a+b  means :
     a=b
     b=old (a) + old (b)
