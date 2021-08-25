ce527355dedf2d766d4893baa11c287df8b1c092
pro-ghanem <pro.ahmedghanem@gmail.com>
2021-08-25 07:26:45 +0200

    Create find_substring.py

---
+++ String Manipulation/find_substring.py
@@ -0,0 +1,4 @@
+# there are 3 ways to do it : https://www.geeksforgeeks.org/python-check-substring-present-given-string/
+1- using string.find("substing") method , and check if it is true on not
+2- using string.count("substring") method , and check if it is > 0 or not 
+3- re.search("substring","string")
