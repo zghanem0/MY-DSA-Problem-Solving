68816fdff596dd42b274dc32832865aff3e0c4ed
Ahmed Ghanem <pro.ahmedghanem@gmail.com>
2024-03-12 18:23:12 +0000

    Create try_and_except.py

---
+++ general/try_and_except.py
@@ -0,0 +1,6 @@
+>> diference between raise,try,assert :
+raise - raise an exception.
+
+assert - raise an exception if a given condition is (or isn't) true.
+
+try - execute some code that might raise an exception, and if so, catch it.
